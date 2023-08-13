870c6603c39530e7b51f709d10cc1f68f08b2ade
Maulen Makhul <makhulov98@mail.ru>
2023-08-13 11:16:22 +0600

    api.php

--- routes/api.php
+++ routes/api.php
@@ -8,21 +8,21 @@ use App\Http\Controllers\Api\GameController;
 use App\Http\Controllers\Api\MatchController;
 
 Route::controller(UserController::class)->prefix('user')->group(function () {
-    Route::post('register/send-code', 'registerSendCode');
-    Route::post('register/check', 'registerCheck');
-    Route::post('register', 'register');
-    Route::post('login', 'login');
-    Route::post('late/auth', 'lateAuth');
-    Route::post('password/reset/send-code','passwordResetSendCode');
-    Route::post('password/reset/check-code', 'passwordResetCheckCode');
-    Route::post('password/reset', 'passwordReset');
+    Route::post('register/send-code', 'registerSendCode')->middleware('auth:sanctum');
+    Route::post('register/check', 'registerCheck')->middleware('auth:sanctum');
+    Route::post('register', 'register')->middleware('auth:sanctum');
+    Route::post('login', 'login')->middleware('auth:sanctum');
+    Route::post('late/auth', 'lateAuth')->middleware('auth:sanctum');
+    Route::post('password/reset/send-code','passwordResetSendCode')->middleware('auth:sanctum');
+    Route::post('password/reset/check-code', 'passwordResetCheckCode')->middleware('auth:sanctum');
+    Route::post('password/reset', 'passwordReset')->middleware('auth:sanctum');
     Route::post('edit', 'edit')->middleware('auth:sanctum');
     Route::post('delete', 'delete')->middleware('auth:sanctum');
 });
 
 Route::controller(ListController::class)->prefix('list')->group(function (){
-   Route::get('cats', 'catsList');
-   Route::get('cities', 'citiesList');
+   Route::get('cats', 'catsList')->middleware('auth:sanctum');
+   Route::get('cities', 'citiesList')->middleware('auth:sanctum');
 });
 
 Route::controller(GameController::class)->prefix('game')->group(function () {
